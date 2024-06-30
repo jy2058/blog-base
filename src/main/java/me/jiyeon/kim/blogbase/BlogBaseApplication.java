cb7343c59fe7ba0843e662b10f1ee45fe79f2c4b
jy2058 <jiy3285@gmail.com>
2024-06-30 16:40:05 +0900

    프로젝트 생성, test

---
+++ src/main/java/me/jiyeon/kim/blogbase/BlogBaseApplication.java
@@ -0,0 +1,13 @@
+package me.jiyeon.kim.blogbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jiyeon/kim/blogbase/BlogBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jiyeon.kim.blogbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
