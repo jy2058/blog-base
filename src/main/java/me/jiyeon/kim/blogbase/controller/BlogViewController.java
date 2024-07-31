1485afe6bbfbd442b0ff9f63873c0b1ce0e1cb78
jy2058 <jiy3285@gmail.com>
2024-07-31 21:54:09 +0900

    블로그 글 수정, 삭제, 등록 view 구현

--- src/main/java/me/jiyeon/kim/blogbase/controller/BlogViewController.java
+++ src/main/java/me/jiyeon/kim/blogbase/controller/BlogViewController.java
@@ -3,10 +3,13 @@ package me.jiyeon.kim.blogbase.controller;
 import lombok.RequiredArgsConstructor;
 import me.jiyeon.kim.blogbase.domain.Article;
 import me.jiyeon.kim.blogbase.dto.ArticleListViewResponse;
+import me.jiyeon.kim.blogbase.dto.ArticleViewResponse;
 import me.jiyeon.kim.blogbase.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,4 +27,25 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
         return "articleList";
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+
 }
